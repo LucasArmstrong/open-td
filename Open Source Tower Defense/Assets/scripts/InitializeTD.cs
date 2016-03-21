d5b1996da338ed12e37ea3f7b11f1cc2e7d370e2
Lucas Armstrong <lucasacode@gmail.com>
2016-03-20 20:42:30 -0400

    First Commit
    This is a test commit, and also can serve to show the progress starting
    from scratch.
    
    Open-TD is a tower defense game being developed in Unity3D with C#.

---
+++ Open Source Tower Defense/Assets/scripts/InitializeTD.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitializeTD : MonoBehaviour {
+
+    [SerializeField]
+    private string VERSION = "0";
+
+    [SerializeField]
+    private string TITLE = "Open Source Tower Defense";
+
+    void Awake()
+    {
+        Debug.Log("(" + VERSION + ") " + TITLE);
+    }
+
+}
